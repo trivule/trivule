2ee609ceb82a089e50cce3b20714e465e6b9f84e
Claude Fassinou <dev.claudy@gmail.com>
2023-06-14 06:00:16 +0200

    testing

--- src/example.ts
+++ src/example.ts
@@ -1,6 +1,4 @@
-import { QvLocal } from "./locale/qv-local";
 import { QvForm, QvInput } from "./validation";
-QvLocal.local("en");
 const qvForm = new QvForm("form", {
   validClass: "success",
 });
