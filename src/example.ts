9dea291674b53ba40ded9b27b9d724cdd2171b27
Claudye <dev.claudy@gmail.com>
2024-05-02 22:35:16 +0100

    examples

--- src/example.ts
+++ src/example.ts
@@ -1,4 +1,20 @@
+import { RuleCallBack } from "./contracts";
 import { TrLocal } from "./locale/tr-local";
-import { TrivuleForm, TrivuleInput } from "./validation";
+import { TrBag, TrivuleForm, TrivuleInput } from "./validation";
 const trInput = new TrivuleForm("#gift-form");
 trInput.init();
+
+const notSudoRule: RuleCallBack = (input: string) => {
+  return {
+    value: input,
+    passes: input != "sudo",
+    type: "text", //optional
+    alias: undefined, //optional
+  };
+};
+TrBag.rule(
+  "notSudo",
+  notSudoRule,
+  "The input value should not bet 'sudo'",
+  "en"
+);
